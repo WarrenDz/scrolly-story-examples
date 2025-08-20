cb656bed35aa592969e9fbfc7f048aa47148c4c2
Warren <warren.r.davison@gmail.com>
2025-08-20 19:55:12 -0400

    initial standup commit

---
+++ config.js
@@ -0,0 +1,17 @@
+function generateScriptConfig() {
+  window.storyMapsEmbedConfig = {
+      storyId: "2c5879f192a648049d032412b1b2701c",
+      rootNode: ".storymaps-root",
+      topOffset: "3rem",
+  };
+}
+
+function createScriptedEmbed() {
+  const script = document.createElement('script');
+  script.id = 'embed-script';
+  script.src = `https://storymaps.arcgis.com/embed/view`;
+  document.body.appendChild(script);
+}
+
+generateScriptConfig();
+createScriptedEmbed();
